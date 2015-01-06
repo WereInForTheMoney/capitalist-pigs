9c2014f8365d6c474dd1010c073a22628c753d54
Loïc Prieto <loic.sephiroth@gmail.com>
2015-01-06 20:46:30 +0100

    Initial commit with the code structure for both the backend and frontend

---
+++ backend/src/main/java/cat/wereinitforthemoney/capitalistpigs/backend/Launcher.java
@@ -0,0 +1,10 @@
+package cat.wereinitforthemoney.capitalistpigs.backend;
+
+/**
+ * @author loic
+ */
+public class Launcher {
+    public static void main(String[] args){
+        System.out.println("Hola holita!");
+    }
+}
